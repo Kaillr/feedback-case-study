f110566abc302c267a15878cd6f46a6a074b5c72
Kaillr <125704518+Kaillr@users.noreply.github.com>
2025-01-21 15:44:40 +0100

    Add header and doc titlte to 404 page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react'
 import Header from "../components/Header";
 
 export default function NotFoundPage() {
+    useEffect(() => {
+        document.title = "404 - Protector Forsikring"
+    })
+
     return (
         <>
             <Header />
-            <h1>Beklager, vi finner ikke siden (404)</h1>
+            <main>
+                <h1>Beklager, vi finner ikke siden (404)</h1>
+            </main>
         </>
     )
 }
